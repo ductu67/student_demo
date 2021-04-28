d4ecb56fb16027d3b39d0a340ac58ef9f1bf2d58
TuND <ngductu.kma@gmail.com>
2021-04-28 10:34:14 -0700

    second commit

--- www/config.php
+++ www/config.php
@@ -1,8 +1,8 @@
 <?php
-   	define('DB_SERVER', 'localhost');
+   	define('DB_SERVER', 'mysql-student');
    	define('DB_USERNAME', 'root');
-   	define('DB_PASSWORD', '');
-   	define('DB_DATABASE', 'sourcecodester_studentportal');
+   	define('DB_PASSWORD', 'A12345678');
+   	define('DB_DATABASE', 'istudent_db');
    	$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    	if(!$connection){
 		die("Connection failed: " . mysqli_connect_error());
